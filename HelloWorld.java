ab838964af897f5369a15d739a07228b98881a25
unknown <luthfiardiansyah@student.telkomuniversity.ac.id>
2020-09-16 19:49:56 +0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String[] args)	{
+		System.out.println("Nama: Luthfi Ardiansyah");
+		System.out.println("NIM: 6706201136");
+		System.out.println("Kelas: D3IF-44-03");
+	}
+}
