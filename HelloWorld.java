ab345c9a365ae13f0ae545eacc63cb3af48ed3c3
luthfiardiansyah01 <71245686+luthfiardiansyah01@users.noreply.github.com>
2021-01-24 20:48:52 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
 public class HelloWorld {
 
 	public static void main (String[] args)	{
-		System.out.println("Nama: Luthfi Ardiansyah");
-		System.out.println("NIM: 6706201136");
-		System.out.println("Kelas: D3IF-44-03");
+		System.out.println("Hello World");
 	}
 }
